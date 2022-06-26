8861e548eae5c6eff766dbacec89ca3a55904350
Anton K <89594975+Pusttak@users.noreply.github.com>
2022-06-26 11:40:22 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import Loader from 'components/Loader';
 import Container from 'components/Container';
@@ -22,6 +22,7 @@ export const App = () => (
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Suspense>
     </Container>
